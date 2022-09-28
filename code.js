11c4781f61125b233739ddd2ec4785022d1f10bf
== <==>
2022-09-28 10:17:15 +0300

    Updating the code.js file and style of certificates

---
+++ code.js
@@ -0,0 +1,12 @@
+function myFunc(){
+    window.open('https://www.credly.com/badges/46ebb039-945a-43c1-b774-7fb083b01882/linked_in_profile','AWS Certified Cloud Practitioner','width=600,height=400')
+}
+function google(){
+    window.open('https://www.credly.com/badges/f0e5f245-04eb-4519-8634-a6d8c9edbfa8?source=linked_in_profile','AWS Certified Cloud Practitioner','width=600,height=400')
+}
+function tableau(){
+    window.open('https://www.coursera.org/account/accomplishments/specialization/certificate/G7ZLY6KQT9UY','AWS Certified Cloud Practitioner','width=600,height=400')
+}
+function sql(){
+    window.open('https://www.coursera.org/account/accomplishments/certificate/B2HR83F2YLTR','AWS Certified Cloud Practitioner','width=600,height=400')
+}
