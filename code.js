6d7da1bdac1e5deda03f845c15ae8aa444d87b90
== <==>
2022-11-30 10:26:24 +0200

    updating CV contents

--- code.js
+++ code.js
@@ -5,7 +5,9 @@ function myFunc(){
     alert(inc);
 }
 
-
+function architect(){
+    window.open('https://www.linkedin.com/learning/certificates/024f93172e394f0d18a6209c8da2f1918314f96fcdd7100ea6d09cf79a3f8d58?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3Bj4u0D6ICTrOvJTv%2FuEP1ug%3D%3D&accountId=92408722&u=92408722&success=true&authUUID=Ncv9ATQ9RUmeGHmUQ7YAlQ%3D%3D','AWS Certified Solutions Architect Associate','width=600,height=400')
+}
 
 function google(){
     window.open('https://www.credly.com/badges/f0e5f245-04eb-4519-8634-a6d8c9edbfa8?source=linked_in_profile','AWS Certified Cloud Practitioner','width=600,height=400')
